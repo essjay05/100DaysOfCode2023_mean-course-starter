377217f9c5b7eb5bef1610ca0d5c78b15895eb9a
Joy Serquiña <stepjoyous@gmail.com>
2023-02-21 13:11:22 -0800

    Updated post-create component files to add click script action successfully.

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -5,5 +5,7 @@ import { Component } from '@angular/core'
   templateUrl: './post-create.component.html'
 })
 export class PostCreateComponent {
-
+  onAddPost = () => {
+    alert('Post was clicked')
+  }
 }
