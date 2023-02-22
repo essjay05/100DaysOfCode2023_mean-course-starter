f0224c166385eea858354cdde9212ff9324713ee
Joy Serquiña <stepjoyous@gmail.com>
2023-02-21 16:16:15 -0800

    Added FormModules to app.modules.ts and used ngModel binding in post-create component successfully.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
@@ -11,7 +12,8 @@ import { PostCreateComponent } from './posts/post-create/post-create.component';
     PostCreateComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -5,10 +5,10 @@ import { Component } from '@angular/core'
   templateUrl: './post-create.component.html'
 })
 export class PostCreateComponent {
+  enteredValue = ''
   newPost = 'No content...'
 
-  onAddPost = (postInput: HTMLTextAreaElement) => {
-    console.dir(postInput)
-    this.newPost = postInput.value
+  onAddPost = () => {
+    this.newPost = this.enteredValue
   }
 }
