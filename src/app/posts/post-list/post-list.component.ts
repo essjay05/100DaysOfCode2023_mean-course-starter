a760b22e5d03335db555617c11919be0e8b9a9f5
Joy Serquiña <stepjoyous@gmail.com>
2023-02-21 17:21:48 -0800

    Removed hard coded posts data and added conditional logic to mat-accordion in post-list html to only appear if posts.length > 0 successfully.

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -7,12 +7,5 @@ import { Component } from "@angular/core";
 })
 
 export class PostListComponent {
-  posts = [
-    {title: `First post title`, content: `This is the first post content, tada!!!....`},
-    {title: `Second post title`, content: `This is the Second post content, tada!!!....`},
-    {title: `Third post title`, content: `This is the Third post content, tada!!!....`},
-    {title: `Fourth post title`, content: `This is the Fourth post content, tada!!!....`},
-    {title: `Fifth post title`, content: `This is the Fifth post content, tada!!!....`},
-    {title: `Sixth post title`, content: `This is the Sixth post content, tada!!!....`}
-  ]
+  posts = []
 }
