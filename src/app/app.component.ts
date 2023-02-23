31b5cf6e41c975c11547dc3abf8e171e5b221c68
Joy Serquiña <stepjoyous@gmail.com>
2023-02-22 21:37:02 -0800

    Created posts with property and event binding to post-create and post-list component. Passed post up to the main app component and then down the storedPosts array down to the post-list component after it got saved to the main app.ts file.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'mean-course-starter';
+
+  storedPosts = []
+
+  onPostAdded(post) {
+    this.storedPosts.push(post)
+  }
 }
--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, Input } from "@angular/core";
 
 @Component({
   selector: 'app-post-list',
@@ -7,5 +7,5 @@ import { Component } from "@angular/core";
 })
 
 export class PostListComponent {
-  posts = []
+  @Input() posts = []
 }
