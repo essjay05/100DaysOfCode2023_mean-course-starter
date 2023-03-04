cf71c83967c9cbc3b0d9c26681b8a94599c746b4
Joy Serquiña <stepjoyous@gmail.com>
2023-03-04 13:14:19 -0800

    Successfully created route for /api/posts and returned posts data.

--- backend/app.js
+++ backend/app.js
@@ -2,13 +2,24 @@ const express = require('express')
 
 const app = express()
 
-app.use((req, res, next) => {
-  console.log('First middleware')
-  next()
-})
+app.use('/api/posts', (req, res, next) => {
+  const posts = [
+    {
+      id: 'asdfqwer1',
+      title: '1st server side post',
+      content: 'This is coming from ther server!'
+    },
+    {
+      id: 'poiqerj2a',
+      title: '2nd server side post',
+      content: 'This is coming from ther server...'
+    }
+  ]
 
-app.use((req, res, next) => {
-  res.send('Hello from express!')
+  res.status(200).json({
+    message: 'Posts feteched successfully!',
+    posts: posts
+  })
 })
 
 module.exports = app
