ab19394cf26bd36b7db96edb2f3d465dc87dd68f
Joy Serquiña <stepjoyous@gmail.com>
2023-03-08 22:45:36 -0800

    Installed body-parser npm package.

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,10 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*')
@@ -12,6 +16,11 @@ app.use((req, res, next) => {
     'GET, POST, PATCH, DELETE, OPTIONS')
   next()
 })
+
+app.post('/api/posts', (req, res, next) => {
+  console.log(`req.body:`)
+  console.log(req.body)
+}) 
 
 app.use('/api/posts', (req, res, next) => {
   const posts = [
